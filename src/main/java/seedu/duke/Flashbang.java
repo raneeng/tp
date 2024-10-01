1674bd66419bac41d8aebd1ce1cb209f2995b251
Paulifyer <ethansoh0308@gmail.com>
2024-10-01 17:42:29 +0800

    Added a scanner for inputs

---
+++ src/main/java/seedu/duke/Flashbang.java
@@ -0,0 +1,13 @@
+package seedu.duke;
+
+import java.util.Scanner;
+
+public class Duke {
+    /**
+     * Main entry-point for the java.duke.Duke application.
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
