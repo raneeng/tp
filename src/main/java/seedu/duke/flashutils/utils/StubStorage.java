38e597fbac141f339cdbfbaddcb1c25c03d74c3e
raneeng <raneengyinyee@gmail.com>
2024-10-31 15:05:48 +0800

    Added unit testing for FlashbangCommand

---
+++ src/main/java/seedu/duke/flashutils/utils/StubStorage.java
@@ -0,0 +1,21 @@
+package seedu.duke.flashutils.utils;
+
+import seedu.duke.flashutils.types.FlashBook;
+
+public class StubStorage extends Storage {
+
+    private boolean isWriteFlashBookToFileCalled = false;
+
+    public StubStorage() {
+        super("./data");
+    }
+
+    @Override
+    public void writeFlashBookToFile(FlashBook flashBook) {
+        isWriteFlashBookToFileCalled = true;
+    }
+
+    public boolean isWriteFlashBookToFileCalled() {
+        return isWriteFlashBookToFileCalled;
+    }
+}
