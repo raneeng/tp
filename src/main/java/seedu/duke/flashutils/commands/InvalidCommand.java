b959adace584e321708c18ad19a9bc48aefacf45
Paulifyer <ethansoh0308@gmail.com>
2024-11-12 01:28:07 +0800

    Change InvalidCommand to addtionally accept String inputs

--- src/main/java/seedu/duke/flashutils/commands/InvalidCommand.java
+++ src/main/java/seedu/duke/flashutils/commands/InvalidCommand.java
@@ -1,8 +1,16 @@
 package seedu.duke.flashutils.commands;
 
 public class InvalidCommand extends Command {
+    private String errorMessage;
+    public InvalidCommand() {
+        this.errorMessage = "uh oh bad command";
+    }
+    public InvalidCommand(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
     @Override
     public CommandResult execute() {
-        throw new IllegalArgumentException("uh oh bad command");
+        throw new IllegalArgumentException(errorMessage);
     }
 }
