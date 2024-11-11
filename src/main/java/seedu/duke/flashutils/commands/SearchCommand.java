25b83063bbf2741d2f2fa3b41cb0e65e5dc4f204
Paulifyer <ethansoh0308@gmail.com>
2024-11-11 16:26:08 +0800

    Add search command

--- src/main/java/seedu/duke/flashutils/commands/SearchCommand.java
+++ src/main/java/seedu/duke/flashutils/commands/SearchCommand.java
@@ -1,4 +1,33 @@
 package seedu.duke.flashutils.commands;
 
-public class SearchCommand {
+import seedu.duke.flashutils.types.Card;
+import seedu.duke.flashutils.types.FlashCardSet;
+
+public class SearchCommand extends Command {
+    private final String searchTerm;
+    private final boolean byTopic;
+    private final FlashCardSet targetSet;
+
+    public SearchCommand(String searchTerm, boolean byTopic, FlashCardSet targetSet) {
+        this.searchTerm = searchTerm;
+        this.byTopic = byTopic;
+        this.targetSet = targetSet;
+    }
+
+    @Override
+    public CommandResult execute() {
+        StringBuilder matchingCards = new StringBuilder();
+        for (Card card : targetSet) {
+            if (byTopic && card.getTopic().contains(searchTerm)) {
+                matchingCards.append(card).append("\n");
+            } else if (card.getAnswer().contains(searchTerm) || card.getQuestion().contains(searchTerm)) {
+                matchingCards.append(card);
+            }
+        }
+        if (matchingCards.isEmpty()) {
+            return new CommandResult("No Cards found :(");
+        } else {
+            return new CommandResult(matchingCards.toString());
+        }
+    }
 }
